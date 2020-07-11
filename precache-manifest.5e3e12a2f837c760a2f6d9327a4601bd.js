be20d292f6dc591b8f8ae77f5bde4a65e317450a
franz-kbarek <franzferdinand.kbarek@gmail.com>
2020-07-12 03:44:08 +0900

    Updates

---
+++ precache-manifest.5e3e12a2f837c760a2f6d9327a4601bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b6564a025e9ffa1d2a88bb59ff3997",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "775a3f903b01deea492c",
+    "url": "/robofriends/static/css/main.134bbf94.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775a3f903b01deea492c",
+    "url": "/robofriends/static/js/main.c6469257.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "0ec244ed0b7337c40ef46cb5daa947b1",
+    "url": "/robofriends/static/media/Thousandyard-3B8X.0ec244ed.woff"
+  }
+]);
