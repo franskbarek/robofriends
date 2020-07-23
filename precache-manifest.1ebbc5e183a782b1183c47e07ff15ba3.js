e3615fa935b87de1d1c4c4d0ee92c9e65bb513ae
franz-kbarek <franzferdinand.kbarek@gmail.com>
2020-07-24 00:20:19 +0900

    Updates

--- precache-manifest.1ebbc5e183a782b1183c47e07ff15ba3.js
+++ precache-manifest.1ebbc5e183a782b1183c47e07ff15ba3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "356dfd05615153850dbd005c5b61a31d",
+    "revision": "26bfdba7c3261446cd56332a28897268",
     "url": "/robofriends/index.html"
   },
   {
-    "revision": "59095690a89922d8283f",
+    "revision": "68029cae79872d730ebf",
     "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "a97836b9ff532a21eee1",
+    "revision": "e33386517d3745e35a2a",
     "url": "/robofriends/static/css/main.134bbf94.chunk.css"
   },
   {
-    "revision": "59095690a89922d8283f",
-    "url": "/robofriends/static/js/2.6d6ecbb4.chunk.js"
+    "revision": "68029cae79872d730ebf",
+    "url": "/robofriends/static/js/2.d2e9c624.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/robofriends/static/js/2.6d6ecbb4.chunk.js.LICENSE.txt"
+    "url": "/robofriends/static/js/2.d2e9c624.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a97836b9ff532a21eee1",
-    "url": "/robofriends/static/js/main.ddb775a9.chunk.js"
+    "revision": "e33386517d3745e35a2a",
+    "url": "/robofriends/static/js/main.cef0837d.chunk.js"
   },
   {
     "revision": "e7d2cbb368e0813da61d",
