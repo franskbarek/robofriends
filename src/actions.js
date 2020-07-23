cc5dc79f470023df8885612b4f99ca9fded99401
franz-kbarek <franzferdinand.kbarek@gmail.com>
2020-07-24 00:08:39 +0900

    refactor fungsi

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,19 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js'
+import { CHANGE_SEARCH_FIELD, 
+         REQUEST_ROBOTS_PENDING,
+         REQUEST_ROBOTS_SUCCESS,
+         REQUEST_ROBOTS_FAILED
+} from './constants.js'
 
 export const setSearchField = (text) => ({
     type: 'CHANGE_SEARCH_FIELD',
     payload: text
 })
+
+export const requestRobots = () => (dispatch) => { //Higer order function
+    dispatch({ type: REQUEST_ROBOTS_PENDING });
+    fetch('htpps://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+    .catch(error => ({ REQUEST_ROBOTS_FAILED, payload: error }))
+}
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,46 +5,42 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 // import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
 
     }
 }
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: []
-        }
-    }
+
 
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }))
+        this.props.onRequestRobots()
     }
 
 
     render() {
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        return !robots.length ? //kodiisi jika server lambat/ masalah!!
-            <h1>Load</h1> :
+        return isPending ? //kodiisi jika server lambat/ masalah!!
+            <h1>Waiting</h1> :
             (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
